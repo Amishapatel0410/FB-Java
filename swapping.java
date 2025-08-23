6e2ea0abe47035c6d7c3696a44346680d5f47fb7
Amisha Patel <amisha.patel_cs24@gla.ac.in>
2025-08-23 21:32:00 +0530

    Add files via upload

---
+++ swapping.java
@@ -0,0 +1,80 @@
+// import java.util.Scanner;
+// public class swapping {
+//     public static void main(String[] args) {
+//       Scanner Sc = new Scanner(System.in);
+//       int a = Sc.nextInt();
+//       int b = Sc.nextInt();
+//       int temp = a;
+//        a = b;
+//        b = temp;
+//        System.out.println("after swapping:a = "+a+ ",b = "+b);    
+//     }    
+// }
+
+// REVERSE NUMBER
+// import java.util.Scanner;
+// public class swapping{
+//   public static void main(String[] args) {
+//      Scanner Sc = new Scanner(System.in);
+//       int number = Sc.nextInt();
+//       int reverse = 0;
+//       while (number != 0){
+//        int digit = number%10;
+//        reverse = reverse*10 + digit;
+//        number /= 10;
+//        System.out.println("reverse = " +reverse);
+
+//       }
+//   }
+// }
+
+
+// import java.util.Scanner;
+// public class swapping{
+//   public static void main(String[] args) {
+//       Scanner Sc = new Scanner(System.in);
+//       int age = Sc.nextInt();
+//       if(age>= 18){
+//         System.out.println("eligible for addmission");
+//       }else{
+//          System.out.println(" not eligible for addmission");
+
+//       }
+//   }
+// }
+
+// EVEN ODD
+// import java.util.Scanner;
+// public class swapping{
+//   public static void main(String[] args) {
+//           Scanner Sc = new Scanner(System.in);
+//       int number = Sc.nextInt();
+//       if(number % 2 == 0){
+//         System.out.println("even number");
+//       }else{
+//          System.out.println("odd number");
+
+//       }
+//   }
+// }
+// import java.util.Scanner;
+// public class swapping{
+//   public static void main(String[] args) {
+//      Scanner Sc = new Scanner(System.in);
+//          int day = Sc.nextInt();
+//           switch(day){
+//           case1:
+//           System.out.println("monday");
+//            break;
+//            case2:
+//           System.out.println("tuesday"); 
+//           break;
+//           case3:
+//           System.out.println("wednesday"); 
+//           break;
+//           default:System.out.println("invalid day");
+//           }
+//         }
+//       }
+
+
